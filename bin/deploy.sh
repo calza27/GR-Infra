5b4bd83681b8d45475738932fbb9684515ec6c97
calza27 <calza27@gmail.com>
2025-01-10 09:51:32 +1100

    Add GSI for sharing Id on the list table

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-die() { echo "${$1:-urgh}" >&2; exit "${$2:-1}"; }
+die() { echo "${1:-urgh}" >&2; exit "${2:-1}"; }
 
 hash aws 2>/dev/null || die "missing dep: aws"
 hash sam 2>/dev/null || die "missing dep: sam"
